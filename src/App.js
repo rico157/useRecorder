68e9b1c415a216b05958f4c44084c786448532e0
rico157 <cogonir@live.com>
2021-05-22 18:39:09 +0100

    ReactMediaRecorder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import useRecorder from "./useRecorder";
 import "./App.css";
+import { useReactMediaRecorder } from "react-media-recorder";
+import { useEffect } from "react";
 
 function App() {
-  let [audioURL, isRecording, startRecording, stopRecording] = useRecorder();
+  const { status, startRecording, stopRecording, mediaBlobUrl } =
+    useReactMediaRecorder({
+      video: false,
+      audio: true,
+      blobPropertyBag: {
+        type: "audio/mp3",
+      },
+    });
+
+  useEffect(() => {
+    (async () => {
+      let stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      });
+      let recorder = new RecordRTCPromisesHandler(stream, {
+        type: "video",
+      });
+      recorder.startRecording();
+
+      const sleep = (m) => new Promise((r) => setTimeout(r, m));
+      await sleep(3000);
+
+      await recorder.stopRecording();
+      let blob = await recorder.getBlob();
+      invokeSaveAsDialog(blob);
+    })();
+  });
+
   return (
     <div className="App">
       {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-      <audio src={audioURL} controls />
-      <button onClick={startRecording} disabled={isRecording}>
+      <audio controls>
+        <source src={mediaBlobUrl} type="audio/mp3"></source>
+      </audio>
+      <button onClick={startRecording} disabled={status === "recording"}>
         start recording
       </button>
-      <button onClick={stopRecording} disabled={!isRecording}>
+      <button onClick={stopRecording} disabled={status === "stopped"}>
         stop recording
       </button>
-      <p>audioURL: {audioURL}</p>
+      <p>status: {status}</p>
+      <p>mediaBlobUrl: {mediaBlobUrl}</p>
+      <p>v: source mp3 fixed</p>
     </div>
   );
 }
